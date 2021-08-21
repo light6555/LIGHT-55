621e36f82216fb600808c40f896fa75d7c5979d3
light6555 <89099527+light6555@users.noreply.github.com>
2021-08-21 11:30:33 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,53 @@
+const help = (pushname,prefix) => {
+return`
+𝐒𝐚𝐥𝐯𝐞 ${pushname} 𝐬𝐞𝐮 𝐥𝐢𝐧𝐝𝐨(a) 
+╔┬━━━━━━━━━━━┈ ❄︎
+││    😎LIGHT-BOT      
+││→CREADOR: LIGHT
+││→𝘯𝘶𝘮𝘦𝘳𝘰: *wa.me/+33660217049*
+││→𝘪𝘯𝘴𝘵𝘢:@joao.witorr
+││→𝘤𝘢𝘯𝘢𝘭:↓↓↓
+││→yt: light
+││→𝘱𝘪𝘹:88993367542 - fale cmg pv caso doe
+││→𝘱𝘳𝘦𝘧𝘪𝘹𝘰: ⌈${prefix}⌋
+││→𝘪𝘯𝘧𝘰 𝘤𝘰𝘷𝘪𝘥-19: ⌈${prefix}covid19⌋
+││𝘤𝘢𝘴𝘰 𝘦𝘯𝘤𝘰𝘯𝘵𝘳𝘦 𝘦𝘳𝘳𝘰𝘴: ⌈${prefix}bugreport⌋
+│╰────────────┈ ❄︎
+⊢🔴──◣*Menus actualizado*◥──🔴✯
+
+ ⊢───◣𝘈𝘵𝘶𝘢𝘭𝘪𝘻𝘢cione𝘴◥───✯
+ │❗${prefix}loja❗
+│⚠️ *loja do bot*
+│❗${prefix}menuvip❗
+│⚠️ *Menu dos vips
+│❗ ${prefix}dinero❗
+│⚠️ *Veja seu dinero*
+│❗${prefix}ajudantes❗
+│⚠️ *Bots 24/7 + creditos*
+ ⊢───◣LIGHT◥───✯
+│
+│⇨ ${prefix}menu2
+│ ⚠️ *Segundo menu*
+│⇨ ${prefix}menufig
+│⚠️ *Menu criar figurinhas*
+│⇨ ${prefix}plays
+│⚠️ *Menu donwload de mp3*
+│⇨ ${prefix}jogos
+│⚠️ *Games pra zoar*
+│⇨ ${prefix}menuadm
+│ ⚠️ *Administração de grupos*
+│⇨ ${prefix}menuvip
+│ ⚠️ *Menu para vips*
+│⇨ ${prefix}skiller
+│ ⚠️ *Comandos que o dono usa*
+│
+╰────⟫LIGHT⟫────┈✯
+©     _*Copyright by LIGHT*_
+®     _*dominando todo⚡*_ 
+╘────────────😎`
+
+}
+
+
+
+exports.help = help
